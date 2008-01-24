a96e16b337e771527efda2b61584963b9a206d1d
Joe Danziger <joe@ticc.net>
2008-01-24 21:02:46 +0000

    add admin to project_users

--- trunk/install/mysql_upgrade.sql
+++ trunk/install/mysql_upgrade.sql
@@ -104,6 +104,7 @@ ALTER TABLE `pt_milestones` MODIFY `description` text default NULL;
 /* pt_project_users */
 ALTER TABLE `pt_project_users` MODIFY `userID` char(35) NOT NULL;
 ALTER TABLE `pt_project_users` MODIFY `projectID` char(35) NOT NULL;
+ALTER TABLE `pt_project_users` ADD `admin` tinyint(1) default NULL;
 ALTER TABLE `pt_project_users` ADD `files` tinyint(1) default NULL;
 ALTER TABLE `pt_project_users` ADD `issues` tinyint(1) default NULL;
 ALTER TABLE `pt_project_users` ADD `msgs` tinyint(1) default NULL;
